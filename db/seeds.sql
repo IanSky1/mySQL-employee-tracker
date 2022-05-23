9d0ae9e3eccb39197fe44e6ed1dd25145ae4fd37
Ian <Ian.Mohn1@gmail.com>
2022-05-23 07:58:16 -0500

    updated seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ VALUES
     ('Accounting'),
     ('Software Development');
 
-INSERT INTO role (title, salary, departments_id)
+INSERT INTO role (title, salary, department_id)
 VALUES
     ("HR Manager", 60000, 1),
     ("HR Specialist", 45000, 1),
@@ -13,4 +13,11 @@ VALUES
     ("Software Engineer", 85000, 3),
     ("Senior Developer", 125000, 3);
 
-INSERT INTO employee (first_name, last_name, role_id )
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Thomas', 'Daenda', 2, NULL),
+    ('Peremil', 'Svare', 1, 1),
+    ('Mark', 'Deleano', 3, NULL),
+    ('Zach', 'Davis', 4, 3),
+    ('John', 'Ruckman', 6, NULL),
+    ('Alex', 'Sivenko', 5, 5);
